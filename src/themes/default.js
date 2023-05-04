1fdde50021f10f48eab1151486cbe3bd02a21cb9
bodthegod <77025894+bodthegod@users.noreply.github.com>
2023-05-04 18:58:21 +0100

    Edit: Layout colours

--- src/themes/default.js
+++ src/themes/default.js
@@ -6,11 +6,11 @@ export default {
   },
   // Colors for layout
   colors: {
-    primary1: "hsl(204,23.8%,95.9%)",
-    background1: "#0F1624",
-    accent1: "hsl(34.9,98.6%,72.9%)",
-    button: "hsl(205.1,100%,36.1%)",
-    background2: "hsl(232.7,27.3%,23.7%)",
+    primary1: "hsl(0, 0%, 98%)",
+    background1: "hsl(220, 12%, 10%)",
+    accent1: "hsl(47, 100%, 61%)",
+    button: "hsl(210, 100%, 50%)",
+    background2: "hsl(220, 15%, 15%)",
   },
   // Breakpoints for responsive design
   breakpoints: {
