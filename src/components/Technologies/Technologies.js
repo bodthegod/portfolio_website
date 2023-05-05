6a882be29bfa04326aebb5647d0c99246dabbaf3
bodthegod <77025894+bodthegod@users.noreply.github.com>
2023-05-05 13:07:18 +0100

    Add: Tech stack information and icons

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,12 +1,48 @@
 import React from 'react';
-import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
+import { DiReact, DiDatabase, DiPython } from 'react-icons/di';
 import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
 
 const Technologies = () =>  (
-  <div>
-    Technologies
-  </div>
+  <Section id="tech">
+    <SectionDivider/>
+    <br/>
+    <SectionTitle>Tech Stack</SectionTitle>
+    <SectionText>I've worked with mutliple technologies with web development. I studied a Diploma in
+       Full-Stack Software Development and I enjoy creating Front-End applications!</SectionText>
+       <List>
+        <ListItem>
+          <DiReact size="3rem"/>
+          <ListContainer>
+            <ListTitle>Front-End</ListTitle>
+            <ListParagraph>
+              Experience With <br />
+              React.js
+            </ListParagraph>
+          </ListContainer>
+        </ListItem>
+        <ListItem>
+          <DiPython size="3rem"/>
+          <ListContainer>
+            <ListTitle>Back-End</ListTitle>
+            <ListParagraph>
+              Experience With <br />
+              Python and Databases
+            </ListParagraph>
+          </ListContainer>
+        </ListItem>
+        <ListItem>
+          <DiDatabase size="3rem"/>
+          <ListContainer>
+            <ListTitle>Full-Stack</ListTitle>
+            <ListParagraph>
+              Experience With <br />
+              API Integration
+            </ListParagraph>
+          </ListContainer>
+        </ListItem>
+       </List>
+  </Section>
 );
 
 export default Technologies;
