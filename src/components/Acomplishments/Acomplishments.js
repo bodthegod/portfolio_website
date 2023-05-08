124db29df0d2dd4a3c587f0ff53d17f767fd4bf5
bodthegod <77025894+bodthegod@users.noreply.github.com>
2023-05-08 15:44:55 +0100

    Add: Accomplishments section

--- src/components/Acomplishments/Acomplishments.js
+++ src/components/Acomplishments/Acomplishments.js
@@ -1,19 +1,36 @@
 import React from 'react';
 
-import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
-import { Box, Boxes, BoxNum, BoxText } from './AcomplishmentsStyles';
+import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import { Box, Boxes, BoxHeader, BoxNum, BoxText } from './AcomplishmentsStyles';
 
 const data = [
-  { number: 20, text: 'Open Source Projects'},
-  { number: 1000, text: 'Students', },
-  { number: 1900, text: 'Github Followers', },
-  { number: 5000, text: 'Github Stars', }
+  { header: 'Passed', text: 'Diploma in Full-Stack Development'},
+  { header: 'Learned', text: 'Necessary Web Development skills'},
+  { header: 'Discovered', text: 'Passion for Front End Development'},
+  { header: 'Progressing', text: 'My knowledge in Modern Technologies'},
 ];
 
 const Acomplishments = () => (
-  <div>
-    Acomplishments
-  </div>
+  <Section>
+    <SectionTitle>Why I Love Coding</SectionTitle>
+      <br />
+      <SectionText>
+      "Success in coding is not about being the best, it's about being consistent. It's about showing up day after day, even when the code doesn't work and the bugs seem impossible to fix. It's about never giving up"
+      </SectionText>
+      <br />
+      <SectionText>
+        Developing software has instilled values in me of determination and perseverance like I have never experienced before, and I love it.
+      </SectionText>
+    <Boxes>
+      {data.map((card, index) => (
+        <Box key={index}>
+          <BoxHeader>{card.header}</BoxHeader>
+          <BoxText>{card.text}</BoxText>
+        </Box>
+      ))}
+    </Boxes>
+  </Section>
+  
 );
 
 export default Acomplishments;
