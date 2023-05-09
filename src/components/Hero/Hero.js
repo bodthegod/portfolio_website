f06f8bbf356abf1aa0fe1e4b3384f844a5bcaef4
bodthegod <77025894+bodthegod@users.noreply.github.com>
2023-05-09 15:23:42 +0100

    Finalise: Hero content

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React from "react";
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import Button from '../../styles/GlobalComponents/Button';
-import { LeftSection } from './HeroStyles';
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import Button from "../../styles/GlobalComponents/Button";
+import { LeftSection } from "./HeroStyles";
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+
+const Hero = () => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Hey there! <br /> I'm Joe Playdon.
+      </SectionTitle>
+      <SectionText>
+        I'm a freshly qualified Junior Software Developer, looking for a position in a forward-thinking business that I can progress with and provide value.
+      </SectionText>
+      <br />
+      <SectionText>
+        Scroll down to read more!
+      </SectionText>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
