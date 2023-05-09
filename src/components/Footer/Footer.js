1f6de7f27d39baa423b661e8d6e405c0e63f40db
bodthegod <77025894+bodthegod@users.noreply.github.com>
2023-05-09 14:53:44 +0100

    Finalise: Footer content

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,14 +1,49 @@
-import React from 'react';
-import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
+import React from "react";
+import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from "react-icons/ai";
 
-import { SocialIcons } from '../Header/HeaderStyles';
-import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTitle, Slogan, SocialContainer, SocialIconsContainer } from './FooterStyles';
+import { SocialIcons } from "../Header/HeaderStyles";
+import {
+  CompanyContainer,
+  FooterWrapper,
+  LinkColumn,
+  LinkItem,
+  LinkList,
+  LinkTitle,
+  Slogan,
+  SocialContainer,
+  SocialIconsContainer,
+} from "./FooterStyles";
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <FooterWrapper>
+      <LinkList>
+        <LinkColumn>
+          <LinkTitle>Call</LinkTitle>
+          <LinkItem href="tel:07904-378-467">07904-378-467</LinkItem>
+        </LinkColumn>
+        <LinkColumn>
+          <LinkTitle>Email Me</LinkTitle>
+          <LinkItem href="mailto:joeplaydon@gmail.com">
+            joeplaydon@gmail.com
+          </LinkItem>
+        </LinkColumn>
+      </LinkList>
+      <SocialIconsContainer>
+        <CompanyContainer>
+          <Slogan>Thank you for reading my story! - Joe Playdon</Slogan>
+        </CompanyContainer>
+        <SocialContainer>
+
+        <SocialIcons href="https://www.linkedin.com/in/joe-playdon-882a58231/">
+          <AiFillLinkedin size="3rem" />
+        </SocialIcons>
+        <SocialIcons href="https://github.com/bodthegod">
+          <AiFillGithub size="3rem" />
+        </SocialIcons>
+        </SocialContainer>
+      </SocialIconsContainer>
+    </FooterWrapper>
   );
 };
 
