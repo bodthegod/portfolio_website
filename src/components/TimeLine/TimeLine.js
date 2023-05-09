77872aba56d91f8d166d31abab81383a6d7c7844
bodthegod <77025894+bodthegod@users.noreply.github.com>
2023-05-09 15:24:21 +0100

    Finalise: TimeLine content and styles

--- src/components/TimeLine/TimeLine.js
+++ src/components/TimeLine/TimeLine.js
@@ -1,51 +1,172 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef, useEffect } from "react";
 
-import { CarouselButton, CarouselButtonDot, CarouselButtons, CarouselContainer, CarouselItem, CarouselItemImg, CarouselItemText, CarouselItemTitle, CarouselMobileScrollNode } from './TimeLineStyles';
-import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import { TimeLineData } from '../../constants/constants';
+import {
+  CarouselButton,
+  CarouselButtonDot,
+  CarouselButtons,
+  CarouselContainer,
+  CarouselItem,
+  CarouselItemImg,
+  CarouselItemText,
+  CarouselItemTitle,
+  CarouselMobileScrollNode,
+} from "./TimeLineStyles";
+import {
+  Section,
+  SectionDivider,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import { TimeLineData } from "../../constants/constants";
 
 const TOTAL_CAROUSEL_COUNT = TimeLineData.length;
 
 const Timeline = () => {
-  // const [activeItem, setActiveItem] = useState(0);
-  // const carouselRef = useRef();
+  const [activeItem, setActiveItem] = useState(0);
+  const carouselRef = useRef();
 
-  // const scroll = (node, left) => {
-  //   return node.scrollTo({ left, behavior: 'smooth' });
-  // }
+  const scroll = (node, left) => {
+    return node.scrollTo({ left, behavior: "smooth" });
+  };
 
-  // const handleClick = (e, i) => {
-  //   e.preventDefault();
+  const handleClick = (e, i) => {
+    e.preventDefault();
 
-  //   if (carouselRef.current) {
-  //     const scrollLeft = Math.floor(carouselRef.current.scrollWidth * 0.7 * (i / TimeLineData.length));
-      
-  //     scroll(carouselRef.current, scrollLeft);
-  //   }
-  // }
+    if (carouselRef.current) {
+      const scrollLeft = Math.floor(
+        carouselRef.current.scrollWidth * 0.7 * (i / TimeLineData.length)
+      );
 
-  // const handleScroll = () => {
-  //   if (carouselRef.current) {
-  //     const index = Math.round((carouselRef.current.scrollLeft / (carouselRef.current.scrollWidth * 0.7)) * TimeLineData.length);
+      scroll(carouselRef.current, scrollLeft);
+    }
+  };
 
-  //     setActiveItem(index);
-  //   }
-  // }
+  const handleScroll = () => {
+    if (carouselRef.current) {
+      const index = Math.round(
+        (carouselRef.current.scrollLeft /
+          (carouselRef.current.scrollWidth * 0.7)) *
+          TimeLineData.length
+      );
+
+      setActiveItem(index);
+    }
+  };
 
   // // snap back to beginning of scroll when window is resized
   // // avoids a bug where content is covered up if coming from smaller screen
-  // useEffect(() => {
-  //   const handleResize = () => {
-  //     scroll(carouselRef.current, 0);
-  //   }
+  useEffect(() => {
+    const handleResize = () => {
+      scroll(carouselRef.current, 0);
+    };
 
-  //   window.addEventListener('resize', handleResize);
-  // }, []);
+    window.addEventListener("resize", handleResize);
+  }, []);
 
   return (
-    <div>
-      Timeline
-    </div>
+    <Section id="about">
+      <SectionDivider/>
+      <br />
+      <SectionTitle>My Journey</SectionTitle>
+      <SectionText>
+        Hello again! Since you're reading this, I should tell you about my
+        journey so far...
+      </SectionText>
+      <br />
+      <SectionText>
+        I have embarked on this journey as I am a career changer, and due to
+        always having a passion for technology and advancing my skills- I decided
+        to take a risk and study for a Diploma in Full-Stack Development.
+      </SectionText>
+      <br />
+      <SectionText>
+        I changed careers because I always felt like something was missing,
+        I would always come home to my PC- knowing that I had the perfect tool
+        at my disposal for learning more within the Tech industry. I had to make
+        the leap and trust my instinct that coding was the way... and boy was I
+        right!
+      </SectionText>
+      <br />
+      <SectionText>
+        Once I started my Diploma, I couldn't get enough- every day I would
+        learn more, smiling the entire time! I even set myself the goal to get
+        my GitHub Streak as high as I could, and at the end of my course, I had
+        reached 46 days straight of coding! (In just my Public Repositories!).
+      </SectionText>
+      <br />
+      <SectionText>
+        This journey taught me to be proud that I can do whatever I put my
+        mind to, even things I thought were impossible!
+      </SectionText>
+      <CarouselContainer ref={carouselRef} onScroll={handleScroll}>
+        <>
+          {TimeLineData.map((item, index) => (
+            <CarouselMobileScrollNode
+              key={index}
+              final={index === TOTAL_CAROUSEL_COUNT - 1}
+            >
+              <CarouselItem
+                index={index}
+                id={`carousel__item-${index}`}
+                active={activeItem}
+                onClick={(e) => handleClick(e, index)}
+              >
+                <CarouselItemTitle>
+                  {item.year}
+                  <CarouselItemImg
+                    width="208"
+                    height="6"
+                    viewBox="0 0 208 6"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      clip-rule="evenodd"
+                      d="M2.5 5.5C3.88071 5.5 5 4.38071 5 3V3.5L208 3.50002V2.50002L5 2.5V3C5 1.61929 3.88071 0.5 2.5 0.5C1.11929 0.5 0 1.61929 0 3C0 4.38071 1.11929 5.5 2.5 5.5Z"
+                      fill="url(#paint0_linear)"
+                      fill-opacity="0.33"
+                    />
+                    <defs>
+                      <linearGradient
+                        id="paint0_linear"
+                        x1="-4.30412e-10"
+                        y1="0.5"
+                        x2="208"
+                        y2="0.500295"
+                        gradientUnits="userSpaceOnUse"
+                      >
+                        <stop stop-color="#e9ff15" />
+                        <stop
+                          offset="0.79478"
+                          stop-color="#e9ff15"
+                          stop-opacity="0"
+                        />
+                      </linearGradient>
+                    </defs>
+                  </CarouselItemImg>
+                </CarouselItemTitle>
+                <CarouselItemText>{item.text}</CarouselItemText>
+              </CarouselItem>
+            </CarouselMobileScrollNode>
+          ))}
+        </>
+      </CarouselContainer>
+      <CarouselButtons>
+        {TimeLineData.map((item, index) => (
+          <CarouselButton
+            key={index}
+            index={index}
+            active={activeItem}
+            onClick={(e) => handleClick(e, index)}
+            type="button"
+          >
+            <CarouselButtonDot active={activeItem} />
+          </CarouselButton>
+        ))}
+      </CarouselButtons>
+      <SectionDivider />
+    </Section>
   );
 };
 
