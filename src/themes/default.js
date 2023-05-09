e31dba21e09b31c819c5c130696b7bfbead775d2
bodthegod <77025894+bodthegod@users.noreply.github.com>
2023-05-09 15:24:37 +0100

    Add: Background gradient

--- src/themes/default.js
+++ src/themes/default.js
@@ -7,7 +7,7 @@ export default {
   // Colors for layout
   colors: {
     primary1: "hsl(0, 0%, 98%)",
-    background1: "hsl(220, 12%, 10%)",
+    background1: "radial-gradient(circle, #1d1d1d, #1e1e1e, #202020, #212121, #232323, #222222, #212121, #202020, #1c1c1c, #181818, #141414, #0f0f0f);",
     accent1: "hsl(47, 100%, 61%)",
     button: "hsl(210, 100%, 50%)",
     background2: "hsl(220, 15%, 15%)",
